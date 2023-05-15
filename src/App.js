0b563c2dc825d26a6f08730116f0bf44c7033688
El Hadrachi Hamza <hamza.elhadrachi@gmail.com>
2023-05-15 15:57:36 +0100

    finished cart functionalities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./component/routes/home/home.component";
 import Navigation from "./component/routes/navigation/navigation.component";
 import Authentication from "./component/routes/authentication/authentication.component";
 import Shop from "./component/routes/shop/shop.component";
+import Checkout from "./component/routes/checkout/checkout.component";
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
